a1a121d834ab233fa1b3dd057344b0bce44913ea
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-03-09 14:39:24 -0300

    edições iniciais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import styles from './styles/app.module.css'
+
 function App(){
   return(
-    <div>
-      <p>Test</p>
+    <div className={styles.container}>
+      <div className={styles.profile}>
+        <img src="https://avatars.githubusercontent.com/u/64551315?s=400&u=adfdfb80343d3add6d596032d8ed8a6cd054f8f0&v=4" alt='avatar do github'/>
+        <div>
+          <h1>Alex Ferreira Santos</h1>
+          <h2>Contato</h2>
+          <p>Email: alexfstos2@gmail.com</p>
+          <p>Github: <a href="https://github.com/Alex-Ferreira-Santos">Alex-Ferreira-Santos</a></p>
+          <p>LinkedIn: <a href="https://www.linkedin.com/in/alex-ferreira-santos-/">alex-ferreira-santos-</a></p>
+        </div>
+        
+      </div>
+      
     </div>
   )
 }
