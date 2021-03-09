30599ac2e60e5f8cbb7479c5532e1e2035069b83
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-03-09 15:01:13 -0300

    preparando layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import styles from './styles/app.module.css'
+import styles from './styles/pages/app.module.css'
+import './styles/global.css'
 
 function App(){
   return(
-    <div className={styles.container}>
-      <div className={styles.profile}>
-        <img src="https://avatars.githubusercontent.com/u/64551315?s=400&u=adfdfb80343d3add6d596032d8ed8a6cd054f8f0&v=4" alt='avatar do github'/>
-        <div>
-          <h1>Alex Ferreira Santos</h1>
-          <h2>Contato</h2>
-          <p>Email: alexfstos2@gmail.com</p>
-          <p>Github: <a href="https://github.com/Alex-Ferreira-Santos">Alex-Ferreira-Santos</a></p>
-          <p>LinkedIn: <a href="https://www.linkedin.com/in/alex-ferreira-santos-/">alex-ferreira-santos-</a></p>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.profile}>
+          <img src="https://avatars.githubusercontent.com/u/64551315?s=400&u=adfdfb80343d3add6d596032d8ed8a6cd054f8f0&v=4" alt='avatar do github' className={styles.img}/>
+          <div className={styles.data}>
+            <h1 className={styles.name}>Alex Ferreira Santos</h1>
+            <h2 className={styles.contact}>Contato</h2>
+            <p className={styles.email}>Email: alexfstos2@gmail.com</p>
+            <p className={styles.github}>Github: <a href="https://github.com/Alex-Ferreira-Santos">Alex-Ferreira-Santos</a></p>
+            <p className={styles.linkedin}>LinkedIn: <a href="https://www.linkedin.com/in/alex-ferreira-santos-/">alex-ferreira-santos-</a></p>
+          </div>
         </div>
-        
       </div>
-      
     </div>
   )
 }
