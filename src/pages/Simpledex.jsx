e0fc378d53fb94c985e3db71c00d7d227857d052
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-03-16 10:58:15 -0300

    app's description

--- src/pages/Simpledex.jsx
+++ src/pages/Simpledex.jsx
@@ -6,8 +6,24 @@ export function Simpledex(){
         <div>
             <main className={styles.container}>
                 <img src={simpledex} alt="Icone do Simpledex" className={styles.image}/> 
-                <h1 className={styles.title}>Simpledex</h1>
-               
+                <div >
+                  <h1 className={styles.title}>Simpledex</h1>
+                    <div>
+                        <h3 className={styles.h3}>Uma pokédex simples com dados de seus pokémons favoritos.</h3>
+                        <p>Simpledex é uma pokédex feita por um fã criada para você poder ver os dados de todos os seus pokémons favoritos.</p>
+                        <p>Na listagem de pokémons é possível filtrá-los por elemento para ser mais fácil de encontrar o pokémon desejado.</p>
+                        <p className={styles.space}>O app contem um modo escuro para as pessoas que preferem.</p>
+                        <h3 className={styles.h3}>A Simpledex contem:</h3>
+                        <ul className={styles.lista}>
+                            <li>Os elementos</li>
+                            <li>Hp, ataque, defesa, ataque-especial, defesa-especial e velocidade inicial</li>
+                            <li>As versões shiny</li>
+                            <li>A lista de movimentos</li>
+                            <li>O nome das habilidades</li>
+                            <li>Até a altura e peso</li>
+                        </ul>
+                    </div>  
+                </div>
             </main>
         </div>
     )
